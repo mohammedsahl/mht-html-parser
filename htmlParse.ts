d2e5bf461479d62076c8bfc0ed8ef32272ee68da
Mohammed Sahl <sahl.kably@gmail.com>
2020-07-08 17:16:53 -0400

    Add html parse feature

--- htmlParse.ts
+++ htmlParse.ts
@@ -1,2 +1,9 @@
 import * as jsdom from 'jsdom'
+import * as fs from 'fs';
 const { JSDOM } = jsdom;
+
+fs.readFile('./test.htm', 'utf8', (err: NodeJS.ErrnoException, data: string) => {
+    // console.log(!err? data: err);
+    const dom: jsdom.JSDOM = new JSDOM(data);
+    console.log(dom.window.document.querySelector("body").textContent);
+})
